97b642cb63f7bb68050556e883c1ff8d85824ce7
mosesgi@163.com <mosesgi@163.com>
2023-02-17 20:50:19 +0800

    holder

--- CharacterLookupMVC/Controllers/WordController.cs
+++ CharacterLookupMVC/Controllers/WordController.cs
@@ -1,4 +1,5 @@
-﻿using CharacterLookupMVC.Models;
+﻿using CharacterLookup.Models;
+using CharacterLookupMVC.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -8,13 +9,7 @@ namespace CharacterLookupMVC.Controllers
     [AllowAnonymous]
     public class WordController : Controller
     {
-        static Dictionary<char, Word> words = new Dictionary<char, Word>();
-
-        public WordController()
-        {
-            Initialize();
-        }
-
+        
         // GET: WordController
         public ActionResult Index()
         {
@@ -30,105 +25,15 @@ namespace CharacterLookupMVC.Controllers
                 return View();
             }
 
-            if (!words.ContainsKey(id[0]))
+            if (!WordsHolder.Words.ContainsKey(id[0]))
             {
                 return View();
             }
 
-            var word = words[id[0]];
+            var word = WordsHolder.Words[id[0]];
 
             return View(word);
         }
-
-        void Initialize()
-        {
-            string[] lines = System.IO.File.ReadAllLines(@"primaryschool.txt");
-
-            Grades currGrade = Grades.One;
-            Semister currSemister = Semister.First;
-            string articlePrefix = string.Empty;
-            foreach (var line in lines)
-            {
-                if (string.IsNullOrWhiteSpace(line))
-                {
-                    continue;
-                }
-                if (line.Contains("年级") && line.EndsWith("册"))
-                {
-                    (currGrade, currSemister) = ExtractGrade(line);
-                }
-                else if (line == "识字" || line == "课文" || line == "园地")
-                {
-                    articlePrefix = line;
-                }
-                else
-                {
-                    int charStartPos = 0;
-                    string article = articlePrefix;
-                    while (char.IsDigit(line[charStartPos]))
-                    {
-                        article += line[charStartPos++];
-                    }
-                    for (int i = charStartPos; i < line.Length; i++)
-                    {
-                        Word word = new Word()
-                        {
-                            Article = article,
-                            Grade = currGrade,
-                            Semister = currSemister,
-                            Character = line[i]
-                        };
-                        if (words.ContainsKey(line[i]))
-                        {
-                            Console.WriteLine($"Something is wrong here: {word}");
-                            continue;
-                        }
-                        words[line[i]] = word;
-                    }
-                }
-            }
-        }
-
-        (Grades, Semister) ExtractGrade(string line)
-        {
-            var gradeInChinese = line.Substring(0, 1);
-            var semisterInChinese = line.Substring(line.Length - 2, 1);
-            Grades grade;
-            Semister semister;
-            switch (gradeInChinese)
-            {
-                case "一":
-                    grade = Grades.One;
-                    break;
-                case "二":
-                    grade = Grades.Two;
-                    break;
-                case "三":
-                    grade = Grades.Three;
-                    break;
-                case "四":
-                    grade = Grades.Four;
-                    break;
-                case "五":
-                    grade = Grades.Five;
-                    break;
-                case "六":
-                    grade = Grades.Six;
-                    break;
-                default:
-                    throw new ArgumentException("Invalid grade!");
-            }
-            if (semisterInChinese == "上")
-            {
-                semister = Semister.First;
-            }
-            else
-            {
-                semister = Semister.Second;
-            }
-            return (grade, semister);
-        }
-
 
         public ActionResult BaiduLink([Bind(Prefix = "id")] string id)
         {
---
+++ CharacterLookupMVC/Models/WordsHolder.cs
@@ -0,0 +1,104 @@
+﻿using CharacterLookupMVC.Models;
+
+namespace CharacterLookup.Models
+{
+    public static class WordsHolder
+    {
+        public static Dictionary<char, Word> Words { get; set; }
+
+        static WordsHolder()
+        {
+            Words = new Dictionary<char, Word>();
+            Initialize();
+        }
+
+        static void Initialize()
+        {
+            string[] lines = System.IO.File.ReadAllLines(@"primaryschool.txt");
+
+            Grades currGrade = Grades.One;
+            Semister currSemister = Semister.First;
+            string articlePrefix = string.Empty;
+            foreach (var line in lines)
+            {
+                if (string.IsNullOrWhiteSpace(line))
+                {
+                    continue;
+                }
+                if (line.Contains("年级") && line.EndsWith("册"))
+                {
+                    (currGrade, currSemister) = ExtractGrade(line);
+                }
+                else if (line == "识字" || line == "课文" || line == "园地")
+                {
+                    articlePrefix = line;
+                }
+                else
+                {
+                    int charStartPos = 0;
+                    string article = articlePrefix;
+                    while (char.IsDigit(line[charStartPos]))
+                    {
+                        article += line[charStartPos++];
+                    }
+                    for (int i = charStartPos; i < line.Length; i++)
+                    {
+                        Word word = new Word()
+                        {
+                            Article = article,
+                            Grade = currGrade,
+                            Semister = currSemister,
+                            Character = line[i]
+                        };
+                        if (Words.ContainsKey(line[i]))
+                        {
+                            Console.WriteLine($"Something is wrong here: {word}");
+                            continue;
+                        }
+                        Words[line[i]] = word;
+                    }
+                }
+            }
+        }
+
+        static (Grades, Semister) ExtractGrade(string line)
+        {
+            var gradeInChinese = line.Substring(0, 1);
+            var semisterInChinese = line.Substring(line.Length - 2, 1);
+            Grades grade;
+            Semister semister;
+            switch (gradeInChinese)
+            {
+                case "一":
+                    grade = Grades.One;
+                    break;
+                case "二":
+                    grade = Grades.Two;
+                    break;
+                case "三":
+                    grade = Grades.Three;
+                    break;
+                case "四":
+                    grade = Grades.Four;
+                    break;
+                case "五":
+                    grade = Grades.Five;
+                    break;
+                case "六":
+                    grade = Grades.Six;
+                    break;
+                default:
+                    throw new ArgumentException("Invalid grade!");
+            }
+            if (semisterInChinese == "上")
+            {
+                semister = Semister.First;
+            }
+            else
+            {
+                semister = Semister.Second;
+            }
+            return (grade, semister);
+        }
+    }
+}
